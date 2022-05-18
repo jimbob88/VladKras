0d746f2fb2c7e2a2026d413acf93e2b22b2dac4d
James Blackburn <blackburnfjames@gmail.com>
2022-05-18 12:07:03 +0100

    Added tree printing + exporting + transpilation

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import transpiler
 import tokenizer
 import argparse
 import logging
+import ete3
 
 logging.basicConfig()
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
+
 
 def parseArguments() -> argparse.Namespace:
     parser = argparse.ArgumentParser(
@@ -54,6 +56,9 @@ def checkArguments(args: argparse.Namespace) -> dict:
     if not args.file.endswith(".vkl"):
         raise ValueError("Filename should end with '.vkl'")
 
+    if args.image:
+        args.treeify = True
+
     return {
         "filename": args.file,
         "transpile": args.transpile,
@@ -67,10 +72,30 @@ if __name__ == "__main__":
 
     logger.info(f"Analysing: {args['filename']}")
 
-    tokensList = tokenizer.tokenizeFile(args['filename'])
-    
+    tokensList: list[tuple] = tokenizer.tokenizeFile(args["filename"])
+
     logger.info(f"tokenList: {tokensList}")
 
-    
+    if args["treeify"]:
+        classedTokensList: list[transpiler.tokenType] = transpiler.treeify(
+            tokens=tokensList
+        )
+        newickTree = transpiler.toNewickTree(
+            classedTokensList.currentNode.connectedLowerNodes
+        )
+        logger.info(f"newickTree: {newickTree}")
 
-    
+    if args["image"]:
+        ts: ete3.TreeStyle = ete3.TreeStyle()
+        ts.show_leaf_name = True
+        ts.scale = 20
+        ts.rotation = 90
+        newickTree.render("Tree.png", w=1830, units="mm", tree_style=ts)
+
+    if args["transpile"]:
+        classTokens = transpiler.cleanInputs(transpiler.tokenify(tokensList))
+        classTokens = transpiler.cleanForLoops(classTokens)
+        classTokens = transpiler.cleanWhile(classTokens)
+        classTokens = transpiler.cleanIfStatement(classTokens)
+        transpiledLines = transpiler.transpile(classTokens)
+        logger.info(transpiler.numberLines(transpiledLines))
